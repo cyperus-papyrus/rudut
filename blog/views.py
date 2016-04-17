8fc1b7852f3636b7bf4e80d898b8d816caa2cb0e
cyperus_papyrus <cdg013@gmail.com>
2016-04-17 23:55:22 +0300

    don't touch it!1

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from .forms import ContactForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print posts
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
