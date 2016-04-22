30f12f74f6ca3be5b8e07dc21d055d355f97e293
cyperus_papyrus <cdg013@gmail.com>
2016-04-22 22:21:00 +0300

    +

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import smart_text
@@ -9,6 +10,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class Category(models.Model):
+    category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.category
+
+    def __unicode__(self):
+        return smart_text(self.category)
+
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -18,8 +29,10 @@ class Post(models.Model):
     image = models.ImageField(upload_to="blog/media/", default='/static/image/140.jpg')
     tags = TaggableManager()
     meta_description = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
     is_in_carousel = models.BooleanField(default=False)
+    image_background = models.ImageField(upload_to="blog/media/", default='/static/image/140.jpg')
+    is_on_mainpage = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     url = models.CharField(max_length=200, unique=True, null=True, blank=True)
 
     def publish(self):
